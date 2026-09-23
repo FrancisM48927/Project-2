8bfb522004099078c750e403646637596ccd676b
Francis Mailom <franzm@udel.edu>
2026-09-23 19:15:48 -0400

    Added ability to turn LEDs on/off

--- src/main.c
+++ src/main.c
@@ -5,4 +5,33 @@
 **********************************************************************/
 #include "stm32f4xx.h"
 
-// Start working Tuesday
+#define SWITCH_PORT GPIOC
+#define LED_PORT GPIOD
+#define ANALOG_PIN 2
+
+void Delay_Count(volatile uint32_t count) 
+{
+    while (count--) 
+    {
+    }
+}
+
+int main(void) 
+{
+    // Enable clock for GIOD (LEDs)
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN;
+
+    // Configurre GPIO pins PD0-PD7 (LEDs) as output
+    LED_PORT->MODER &= ~(0xFFFF);
+    LED_PORT->MODER |= (0x5555);
+
+    // Configure PC8 - PC11 (Switches) as inputs
+    SWITCH_PORT->MODER &= ~(0x00FF0000);
+
+    while(1) 
+    {
+        // Read switches & copy status to LEDs
+        uint32_t switches = (SWITCH_PORT->IDR >> 8) & 0x0F;
+        LED_PORT->ODR = (LED_PORT->ODR & ~0x0F) | switches;
+    }
+}
